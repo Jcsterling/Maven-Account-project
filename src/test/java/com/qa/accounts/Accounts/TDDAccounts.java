88640a71fc4bb214cc522ea9ab3049662648f7da
jcsterling <jc1sterling@gmail.com>
2018-08-08 18:58:57 +0100

    broken test code that will fail

--- src/test/java/com/qa/accounts/Accounts/TDDAccounts.java
+++ src/test/java/com/qa/accounts/Accounts/TDDAccounts.java
@@ -1,23 +1,23 @@
-//package com.qa.accounts.Accounts;
-//
-//import org.junit.Test;
-//
-//import junit.framework.Assert;
-//
-//public class TDDAccounts {
-//
-//	
-//	@Test
-//	public void amountOfFirstNamesTest() {
-//		//create 2 accounts with same first name
-//		ServiceClass service = new ServiceClass();
-//		service.addNewAcc("jim", "bob", "1738");
-//		service.addNewAcc("jim", "low", "1414");
-//				
-//		service.getFirstNameCount("jim");
-//		//assert the value (2) if firstname is jim
-//		Assert.assertEquals(2, service.getFirstNameCount("jim"));
-//	}
-//	
-//
-//}
+package com.qa.accounts.Accounts;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TDDAccounts {
+
+	
+	@Test
+	public void amountOfFirstNamesTest() {
+		//create 2 accounts with same first name
+		ServiceClass service = new ServiceClass();
+		service.addNewAcc("jim", "bob", "1738");
+		service.addNewAcc("jim", "low", "1414");
+				
+		service.getFirstNameCount("jim");
+		//assert the value (2) if firstname is jim
+		Assert.assertEquals(2, service.getFirstNameCount("jim"));
+	}
+	
+
+}
